28318a80b605775dd3f938b00bb611d064ce5640
Matthew Wilcoxson <bitbucket@akademy.co.uk>
2017-08-24 15:11:39 +0100

    Check response and already posting

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,32 +4,39 @@ import uuid from 'react-native-uuid'
 //import VersionNumber from 'react-native-version-number';
 import config from "../config/config";
 
+let api = {
+	requestSetup : ( randomUuid ) => {
+		/*
+		request: {
+			appOs: OS System
+			appVersion: current app version,
+			key: server generate uuid,
+			randomUuid: app generate uuid,
+			requestId: request generated uuid,
+		};
+		result: {
+			resultId: must match requestId,
+		};
+		*/
+		let request = {
+			appOs: Platform.OS, // 'ios' or 'android'
+			appVersion: "0.0.1", // TODO: VersionNumber.appVersion + "." + VersionNumber.buildVersion,
 
-export const requestSetup = ( randomUuid ) => {
-	/*
-	request: {
-		appOs: OS System
-		appVersion: current app version,
-		key: server generate uuid,
-		randomUuid: app generate uuid,
-		requestId: request generated uuid,
-	};
-	result: {
-		resultId: must match requestId,
-	};
-	*/
-	let request = {
-		appOs: Platform.OS, // 'ios' or 'android'
-		appVersion: "0.0.1", // TODO: VersionNumber.appVersion + "." + VersionNumber.buildVersion,
+			key: config.local.api.key,
 
-		key: config.local.api.key,
+			randomUuid,
 
-		randomUuid,
+			requestId: uuid.v4(),
 
-		requestId: uuid.v4(),
+			payload : null
+		};
 
-		payload : null
-	};
+		return request;
+	},
 
-	return request;
-}
+	responseCheck : ( response, requestId ) => {
+		return response.responseId = requestId;
+	}
+};
+
+export default api;
--- src/store/actions/profile.js
+++ src/store/actions/profile.js
@@ -1,7 +1,7 @@
 import { REDUCER as R } from '../../constants'
 import { addError } from '../actions'
 import config from '../../config/config'
-import { requestSetup } from '../../lib/api'
+import api from '../../lib/api'
 
 
 export const setRandomUuid = (payload) => ({ type: R.SET_RANDOM_UUID, payload });
@@ -15,36 +15,43 @@ export const postingProfileComplete = () => ({ type: R.POSTING_PROFILE_COMPLETE 
 // thunk
 export const postProfile = () => (dispatch, getState) => {
 
-	dispatch( postingProfile() );
-
 	const state = getState();
 
-	let request = requestSetup( state.profile.randomUuid);
-	request.payload = state.profile;
+	if( !state.profile.postingProfile ) {
 
-	fetch(config.local.api.url + "/api/pre/", {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify( request )
-	})
-		.then(
-			(response) => (
-				response.json()
+		dispatch(postingProfile());
+
+		let request = api.requestSetup(state.profile.randomUuid);
+		request.payload = state.profile;
+
+		fetch(config.local.api.url + "/api/pre/", {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify( request )
+		})
+			.then(
+				(response) => (
+					response.json()
+				)
 			)
-		)
-		.then(
-			(data) => {
-				dispatch( postingProfileComplete() );
-				//dispatch( setCount( data.count ) );
-			}
-		)
-		.catch(
-			(error) => {
-				dispatch( addError( error.message ) );
-				dispatch( postingProfileCancel() );
-			}
-		);
+			.then(
+				(data) => {
+					if( api.responseCheck() ) {
+						if ( data.payload.updated ) {
+							// TODO: dispatch( profileSync )
+						}
+					}
+					dispatch(postingProfileComplete());
+				}
+			)
+			.catch(
+				(error) => {
+					dispatch(addError(error.message));
+					dispatch(postingProfileCancel());
+				}
+			);
+	}
 };
