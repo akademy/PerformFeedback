0cd53ac95e9ed57e534fc838d4d050a9d74e03cf
Matthew Wilcoxson <bitbucket@akademy.co.uk>
2017-08-28 17:07:16 +0100

    Check on app start for sync status

--- src/store/sync.js
+++ src/store/sync.js
@@ -1,11 +1,30 @@
 import { SYNC_STATUS as SS } from '../constants'
 import {postProfile} from "../store/actions/profile";
+import {postLive} from "../store/actions/live";
 
 let sync = {
 	check: ( store ) => {
-		if( store.getState().profile.syncStatus === SS.NOT_SYNCED ) {
+		const state = store.getState();
+
+		// Profile
+		if( state.profile.syncStatus === SS.NOT_SYNCED ) {
 			store.dispatch( postProfile() )
 		}
+
+		// Feedbacks
+		let feedbackSync = SS.SYNCED;
+
+		for( let i=0, z=state.live.feedbacks.length; i<z; i++ ) {
+			if( state.live.feedbacks[i].syncStatus === SS.NOT_SYNCED ) {
+				feedbackSync = SS.NOT_SYNCED;
+				break;
+			}
+		}
+
+		if( feedbackSync === SS.NOT_SYNCED ) {
+			store.dispatch( postLive() );
+		}
+
 	}
 };
 
