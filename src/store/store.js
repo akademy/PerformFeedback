1a4b85b549312ad8c14e42c17d8ddb64fd689a77
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2017-09-02 15:50:43 +0100

    Add redux logger options for better display in IntelliJ

--- src/store/store.js
+++ src/store/store.js
@@ -2,20 +2,36 @@ import { compose, createStore, applyMiddleware } from "redux";
 import {autoRehydrate} from 'redux-persist'
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import { Console } from "../lib/console"
+import { Console, ConsoleDummy } from "../lib/console"
 
 import reducers from './reducers'
 
 let middleware = [thunkMiddleware];
 if( __DEV__ ) {
 	const loggerMiddleware = createLogger({
-		logger: Console,
-		collapsed: true
+		//logger: Console,//ConsoleDummy,//
+		collapsed: true,
+		colors: {
+			title : () => (false),
+			prevState : () => (false),
+			action : () => (false),
+			nextState : () => (false),
+			error : () => (false)
+		},
+		level: {
+
+			prevState : () => (false),
+			action : () => ('log'),
+			nextState : () => (false),
+			error : () => ('log'),
+
+		},
+		logErrors: false
 	});
 	middleware.push(loggerMiddleware);
 }
 
-const store = (initialState: Object={}) => createStore(
+const store = (initialState={}) => createStore(
 	reducers,
 	initialState,
 	compose(
