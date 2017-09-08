f831c14ebfa0e5d0fb97f1d2a13dd83f7e8cab19
Matthew Wilcoxson <bitbucket@akademy.co.uk>
2017-09-08 01:03:29 +0100

    add dev url and switch when not dev

--- src/config/config.local.template.js
+++ src/config/config.local.template.js
@@ -1,8 +1,13 @@
 const configLocal = {
 	api: {
 		key : /\s/,  // e.g. 00000000-0000-4000-0000-00000000000
-		url: /\s/  // e.g. 'http://127.0.0.1:40080'
+		url: /\s/,  // e.g. 'http://127.0.0.1:40080'
+		devUrl: /\s/  // e.g. 'http://127.0.0.1:40080'
 	}
 };
+
+if( __DEV__ ) {
+	configLocal.api.url = configLocal.api.devUrl;
+}
 
 export default configLocal;
